0efa10c708584c65de78775f36c69b42a528ec9c
Cheer.L <lcr19960717@gmail.com>
2021-03-14 00:22:12 +0800

    1s

--- user.py
+++ user.py
@@ -163,7 +163,7 @@ class User:
             symbols = ','.join([target.symbol for target in targets])
             self.trade_client.cancel_orders(symbols, [order.id for order in open_orders])
             logger.info(f'User {self.account_id} cancel all open sell orders')
-            time.sleep(0.5)
+            time.sleep(1)
             sell_amount = [float(order.amount) for order in open_orders]
             target_dict = {
                 target.symbol:target
