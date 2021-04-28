14d086b76015fd304aa6d999023ed9676fc75ccb
Cheer.L <lcr19960717@gmail.com>
2021-04-28 13:46:05 +0800

    check

--- dataset/run.py
+++ dataset/run.py
@@ -1,5 +1,6 @@
 from dataset.redis import Redis
-from dataset.pgsql import get_session, Session, Target, get_trade_from_redis, get_day
+from dataset.pgsql import get_Trade, get_session, Session, Target, get_trade_from_redis, get_day, MS_IN_DAY
+from sqlalchemy import func
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import sys
 
@@ -42,9 +43,45 @@ def vacuum(session: Session=None, table: str=''):
         with get_session() as session:
             _vacuum(table)
 
+def delete_many(session: Session, table:str, ids: 'list[int]'):
+    ids_str = ','.join(ids)
+    sql = f'DELETE FROM {table} WHERE id IN ({ids_str})'
+    session.execute(sql)
+
+def move_trade(Trade, session, start_day, end_day):
+    for day in range(start_day, end_day):
+        DayTrade = get_Trade(day)
+        start = str(day * MS_IN_DAY)
+        end = str((day + 1) * MS_IN_DAY)
+
+        while True:
+            trades = session.query(Trade).filter(Trade.ts >= start, Trade.ts < end).limit(1000).all()
+            if not trades:
+                vacuum(session, Trade.__tablename__)
+                break
+
+            new_trades = [DayTrade.from_trade(trade) for trade in trades]
+            session.bulk_save_objects(new_trades)
+            delete_many(session, Trade.__tablename__, [str(trade.id) for trade in trades])
+            session.commit()
+
 def check_trade_tables():
     with get_session() as session:
         tables = [name for name in session.bind.table_names() if name.startswith('trade')]
+        for table in tables:
+            day = int(table.split('_')[1])
+            Trade = get_Trade(day)
+            min_ts = float(session.query(func.min(Trade.ts)).scalar())
+            if min_ts < day * MS_IN_DAY:
+                min_day = int(min_ts // MS_IN_DAY)
+                move_trade(Trade, session, min_day, day)
+
+            max_ts = float(session.query(func.max(Trade.ts)).scalar())
+            if max_ts > (day + 1) * MS_IN_DAY:
+                max_day = int(max_ts // MS_IN_DAY)
+                move_trade(Trade, session, day+1, max_day+1)
+
+            vacuum(session, Trade.__tablename__)
 
 def main():
     if len(sys.argv) > 1:
@@ -54,6 +91,8 @@ def main():
         elif arg == 'vacuum':
             table = sys.argv[2] if len(sys.argv) > 2 else ''
             vacuum(table=table)
+        elif arg == 'check':
+            check_trade_tables()
 
 if __name__ == '__main__':
     main()
--- dataset/sql_day.py
+++ dataset/sql_day.py
@@ -40,38 +40,3 @@ ORDER BY
   CASE WHEN direction='buy' THEN price END {'DESC' if not reverse else 'ASC'},
   CASE WHEN direction='sell' THEN price END {'DESC' if reverse else 'ASC'}
 LIMIT {limit}'''
-
-def delete_many(session: Session, ids: 'list[int]'):
-    ids_str = ','.join(ids)
-    sql = f'DELETE FROM trade WHERE id IN ({ids_str})'
-    session.execute(sql)
-
-def main():
-    now_day = int(time.time() * 1000 // MS_IN_DAY)
-
-    with get_session() as session:
-        Trade = get_Trade(0)
-        min_ts = float(session.query(func.min(Trade.ts)).scalar())
-        min_day = int(min_ts // MS_IN_DAY)
-        # min_day = 18745
-
-        for day in range(min_day, now_day+1):
-            print(day)
-            DayTrade = get_Trade(day)
-            start = str(day * MS_IN_DAY)
-            end = str((day + 1) * MS_IN_DAY)
-
-            while True:
-                trades = session.query(Trade).filter(Trade.ts >= start, Trade.ts < end).limit(1000).all()
-
-                print(len(trades))
-                if not trades:
-                    break
-
-                new_trades = [DayTrade.from_trade(trade) for trade in trades]
-                session.bulk_save_objects(new_trades)
-                delete_many(session, [str(trade.id) for trade in trades])
-                session.commit()
-
-if __name__ == '__main__':
-    main()
